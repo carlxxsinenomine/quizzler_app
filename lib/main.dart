32e81ebd7dd5d2723070bb4b1969cd1c0c242527
est_calexx <10johannesmunoz@gmail.com>
2025-09-14 12:55:42 +0800

    ADD: Added methods and functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -25,6 +25,22 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = <Icon>[];
+
+  List<String> questions = <String>[
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  List<bool> answers = <bool>[
+    false,
+    true,
+    true,
+  ];
+
+  int questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,6 +80,25 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  if(answers[questionIndex]) {
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        )
+                    );
+                  } else {
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        )
+                    );
+                  }
+
+                });
+                questionIndex = Random().nextInt(questions.length);
               },
             ),
           ),
@@ -84,14 +119,37 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  if(!answers[questionIndex]) {
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        )
+                    );
+                  } else {
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        )
+                    );
+                  }
+                });
+                questionIndex = Random().nextInt(questions.length);
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
+
+
 }
 
 /*
